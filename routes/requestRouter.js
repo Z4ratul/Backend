058e1465a45302e052db9b399adc2ca5b245e69c
Z4ratul <62891744+Z4ratul@users.noreply.github.com>
2024-06-11 19:51:59 +0500

    Update requestRouter.js

--- routes/requestRouter.js
+++ routes/requestRouter.js
@@ -5,7 +5,7 @@ const requestController = require('../controllers/requestController')
 router.post('/', requestController.create)
 router.post('/new', requestController.createAll)
 router.get('/:PartnerId', requestController.findAll)
-router.get('/web', requestController.findAllWeb)
+router.get('/partner/web', requestController.findAllWeb)
 router.get('/active/:PartnerId', requestController.getAllActive)
 
 module.exports = router
